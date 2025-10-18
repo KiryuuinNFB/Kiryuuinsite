180a7dffd4214f1b07c7fca4657234738d66e4bb
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-10-18 20:19:08 +0700

    added essentials

---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { toggleMode } from 'mode-watcher';
+</script>
+
+<div class="min-h-screen bg-background font-[sarabun]">
+    
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -7,6 +8,14 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai+Looped&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
+
+<ModeWatcher />
 
 {@render children?.()}
